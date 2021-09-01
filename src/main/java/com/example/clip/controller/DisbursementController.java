f9e35991a40c6d6c4fd79d6431a86949ad1633aa
jra124_nike <Jose.Ramonblanco+nike@nike.com>
2021-09-01 17:08:23 -0500

    remove unnecesary endpoint

--- src/main/java/com/example/clip/controller/DisbursementController.java
+++ src/main/java/com/example/clip/controller/DisbursementController.java
@@ -1,14 +1,12 @@
 package com.example.clip.controller;
 
 import com.example.clip.model.User;
-import com.example.clip.response.UserDisbursementResponse;
 import com.example.clip.service.DisbursementService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,12 +27,5 @@ public class DisbursementController {
     @GetMapping
     public ResponseEntity<List<User>> triggerBulkDisbursement() {
         return new ResponseEntity<>(disbursementService.processAllDisbursements(), HttpStatus.OK);
-    }
-
-    @GetMapping("/userId")
-    public ResponseEntity<UserDisbursementResponse> getDisbursementByUserId(@PathVariable String userId) {
-        //TODO
-        /*Make the same process as above but for a particular userId*/
-        return new ResponseEntity<>(new UserDisbursementResponse(), HttpStatus.OK);
     }
 }
